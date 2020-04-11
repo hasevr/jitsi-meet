2ab3a8bfde3c75af0eb3d7cedb96c5a4dae14d81
Guo Zile <zlguo1996@gmail.com>
2020-04-11 20:46:05 +0900

    add map component (WIP)

--- react/features/map/components/map.js
+++ react/features/map/components/map.js
@@ -1,7 +1,15 @@
 // @flow
+/* eslint-disable semi, require-jsdoc, no-unused-vars, valid-jsdoc, react/no-multi-comp, new-cap */
 
-import { Components } from 'react';
+import React, { Components } from 'react';
 import * as PoseTypes from '../../../../modules/pose/types';
+import { Provider } from 'react-redux';
+
+import { Avatar } from '../../base/avatar';
+
+const PARTICIPANT_RADIUS = 0.5
+
+declare var APP: Object
 
 type Props = {
     terrain: PoseTypes.Terrain,
@@ -12,9 +20,46 @@ type Props = {
 
 /**
  * Map for showing:
- * 1. terrain of the conference hall
- * 2. pose of every participants
+ * 1. Terrain of the conference hall
+ * 2. Pose of every participants.
  */
-export class Map extends Components<Props> {
+export function Map(props: Props) {
+    return (
+        <div className = 'App'>
+            <svg
+                viewBox = { `0 0 ${props.terrain.width} ${props.terrain.height}` }
+                xmlns = 'http://www.w3.org/2000/svg'>
+                {Object.values(props.participants).map(participant => Participant({
+                    participant
+                }))}
+            </svg>
+        </div>
+    )
+}
 
+type ParticipantProps = {
+    participant: PoseTypes.Participant
+}
+function Participant(props: ParticipantProps) {
+    return (
+        <svg
+            cx = { `${props.participant.pose.position[0]}` }
+            cy = { `${props.participant.pose.position[1]}` }>
+            <circle
+                cx = '0'
+                cy = '0'
+                r = { `${PARTICIPANT_RADIUS}` } />
+            <foreignObject
+                height = { `${PARTICIPANT_RADIUS * 2}` }
+                width = { `${PARTICIPANT_RADIUS * 2}` }
+                x = { `${-PARTICIPANT_RADIUS}` }
+                y = { `${-PARTICIPANT_RADIUS}` } >
+                <Provider store = { APP.store }>
+                    <Avatar
+                        className = 'userAvatar'
+                        participantId = { props.participant.id } />
+                </Provider>
+            </foreignObject>
+        </svg>
+    )
 }
